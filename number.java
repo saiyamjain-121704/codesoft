b6f0c2014ecbde4f8c2eb3ffb56eb64ab1db8965
saiyamjain-121704 <jain20sanjeev@gmail.com>
2025-07-27 19:59:18 +0530

    Add files via upload

---
+++ number.java
@@ -0,0 +1,47 @@
+
+import java.util.Scanner;
+class number
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("generate a random number");
+        int my_num=sc.nextInt();
+        System.out.println("please enter your choice");
+        int choice =sc.nextInt();
+        while(my_num>0 && my_num<=100)
+        {
+        if(choice==my_num)
+        {
+            System.out.println("you have guessed a right number");
+        }
+        else if(choice>my_num){
+            if(choice>(my_num+40))
+            {
+                System.out.println("you have guessed much larger number");
+            }
+            else
+            {
+                System.out.println("your choice is closer");
+            }
+        }
+        else if(choice<my_num)
+        {
+            if(choice<(my_num-40))
+            {
+              System.out.println("you have guessed much smaller number");
+            }
+            else{
+                System.out.println("your choice is closer");
+            }
+        }
+        else{
+            if(choice>100)
+            {
+            System.out.println("guessed number is out of range");
+        }
+    }
+    break;
+}
+}
+}
